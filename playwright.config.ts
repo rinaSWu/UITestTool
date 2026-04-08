e234b8e4d852ccd0c1274b7a9ca8c0b11057a8e5
Rina Sito Wulandari <Rina_SW815@mitrais.com>
2026-04-08 11:36:45 +0700

    locally?

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,15 +6,18 @@ export default defineConfig<EyesFixture>({
   reporter: '@applitools/eyes-playwright/reporter',
 
   use: {
-    /* Configuration for Eyes VisualAI */
     eyesConfig: {
-      /* The following and other configuration parameters are documented at: <url> */
-      apiKey: 'DXKuDa7V2Wd7CViH6RScbDqH1vXzuTFZOPA8uHYW8f4110', // alternatively, set this via environment variable APPLITOOLS_API_KEY
-
+      apiKey: process.env.APPLITOOLS_API_KEY,
     },
-
     baseURL: 'http://localhost:4200/',
     viewport: { height: 720, width: 1280 }
+  },
+
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:4200',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120000,
   },
 
   projects: [
