ca30268b9af92f55108ead26a17db6834574d9fe
Rina Sito Wulandari <Rina_SW815@mitrais.com>
2026-04-09 09:33:46 +0700

    updated config

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,8 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     process.env.CI ? ['dot'] : ['list'],
-    ['@argos-ci/playwright/reporter'],
+    ['@argos-ci/playwright/reporter',    ],
+    
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
